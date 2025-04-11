25d62e164cde41deda5801e44bac15a1f544ebc8
marthagonzaleschumacero <marthagonzales.sis24ch@tecba.edu.bo>
2025-04-10 21:38:14 -0400

    cambios

---
+++ main.py
@@ -0,0 +1,14 @@
+from calculadorapoo import Calculadora
+
+Calculadora_1 = Calculadora(15, 5)
+print(Calculadora_1.sumar())
+
+Calculadora_2 = Calculadora(20, 5)
+print(Calculadora_2.restar())
+
+print(Calculadora_1.multiplicar())
+
+print(Calculadora_2.dividir())
+
+
+
